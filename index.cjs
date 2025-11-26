95782a34aecf268b48290f46fbf948636df415e0
shahzamina <shahzamina>
2025-11-26 11:33:30 +0500

    lost

--- index.cjs
+++ index.cjs
@@ -25,11 +25,11 @@ const adminRoutes=require('./routes/admroute.cjs')
 app.use("/api", adminRoutes);
 
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get(/^\/.*$/, (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
+// app.get(/^\/.*$/, (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+// });
 
 
 
